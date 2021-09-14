f1d77d7b9ba4eebf6b0dd30931f69393ebc5f9c3
prashant <prashant.elex.eng@gmail.com>
2021-09-14 09:52:37 +0530

    first commit

---
+++ src/main/java/lambda/Handler.java
@@ -0,0 +1,22 @@
+package lambda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Handler implements RequestHandler<String, String> {
+
+	private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	public String handleRequest(String event, Context context) {
+		logger.info("event => ", gson.toJson(event));
+		logger.info("context => ", gson.toJson(context));
+		//String response = input;		
+		return "";
+	}
+}
