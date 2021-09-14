6dbe5cc8ea8c35d71785b07156985f34434abffb
prashant <prashant.elex.eng@gmail.com>
2021-09-14 15:07:05 +0530

    Implemented LambdaLogger for successful logging

--- src/main/java/lambda/Handler.java
+++ src/main/java/lambda/Handler.java
@@ -2,29 +2,25 @@ package lambda;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Handler implements RequestHandler<Map<String, String>, String> {
 
-	private static final Logger logger = LoggerFactory.getLogger(Handler.class);
 	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 	public Handler() {
 	}
 
 	public String handleRequest(Map<String, String> event, Context context) {
-		//logger.info("event => ", gson.toJson(event));
-		//logger.info("context => ", gson.toJson(context));
 		
-		System.out.println("event => " + event);
-		System.out.println("event => " + gson.toJson(event));
-		System.out.println("context => " + gson.toJson(context));
+		LambdaLogger logger = context.getLogger();
+
+		logger.log("event => " + gson.toJson(event));
+		logger.log("context => " + gson.toJson(context));
 		
 		// String response = input;
 		return "";
