c52e72a000146c4b1e0eac7f6839541bc962e087
prashant <prashant.elex.eng@gmail.com>
2021-09-14 14:42:28 +0530

    Fixed No class found error using maven-shade plugin

--- src/main/java/lambda/Handler.java
+++ src/main/java/lambda/Handler.java
@@ -12,11 +12,14 @@ public class Handler implements RequestHandler<String, String> {
 
 	private static final Logger logger = LoggerFactory.getLogger(Handler.class);
 	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-	
+
+	public Handler() {
+	}
+
 	public String handleRequest(String event, Context context) {
 		logger.info("event => ", gson.toJson(event));
 		logger.info("context => ", gson.toJson(context));
-		//String response = input;		
+		// String response = input;
 		return "";
 	}
 }
